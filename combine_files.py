cbf6ad47637faa8501c3769df13578a56982fabd
Tim Kappe <tim.kappe@rwth-aachen.de>
2024-05-15 16:37:22 +0200

    made combining rechits more efficient (but cannot preselect while combining anymore)

--- combine_files.py
+++ combine_files.py
@@ -1,29 +1,13 @@
 import numpy as np
+import argparse
+
+import numpy as np
 import pandas as pd
 import argparse
 
 from typing import List, Tuple, Optional, Sequence, Any
 from numpy.typing import NDArray
 from mytypes import Filename, Mask
-
-class Preselector:
-    def __init__(self, preselectionfile: Filename):
-        self.preselection: Mask = np.load(preselectionfile)
-        self.current_idx: int = 0
-        self.max_idx: int = len(self.preselection) - 1
-
-    def _update_idx(self, idx: int):
-        self.current_idx = idx
-
-    def get_part(self, start: int, stop: int) -> Mask:
-        return self.preselection[start:stop]
-
-    def get_next_segment(self, steps: int) -> Mask:
-        next_index: int = self.current_idx + steps
-        out = self.get_part(self.current_idx, next_index)
-        self._update_idx(next_index)
-        return out
-
 
 def file_ending(filename: Filename) -> str:
     return filename.split('.')[-1]
@@ -56,63 +40,49 @@ def combine_dataframes(file_list: Sequence[Filename], savename: Filename) -> Non
 
 def combine_rechits(file_list: Sequence[Filename], savename: Filename) -> None:
     print(f'combining {len(file_list)} files')
-    all_data: List[NDArray] = []  # to be appended
+    all_data: List = []  # to be appended
     for i, file in enumerate(file_list):
         print(f'{i+1:>3}/{len(file_list)}: {file}')
-        all_data += np.load(file).tolist()
-    print(f"INFO: saving as: {savename}")
-    np.save(savename, np.array(all_data))
+        data = np.memmap(file, dtype=np.float32, mode='r')  # cannot specify shape because I do not know the number of photons in the file yet
+        # first 32 positions are meta information because I did not specify the shape
+        # need to get rid of them and get the shape back to 32x32 images
+        num_photons = int((len(data)-32)/32/32)
+        shape = (num_photons, 32, 32)
+        data = data[32:].reshape(shape)
+        all_data += [data]
+    
+    large_array = np.vstack(all_data)
+    print('files combined, starting saving...')
+    np.save(savename, large_array)
+    print(f"INFO: saved as: {savename}")
 
 
-def combine_rechits_preselected(file_list: Sequence[Filename], savename: Filename, preselector: Preselector) -> None:
-    print(f'combining {len(file_list)} files')
-    all_data: List[NDArray] = []  # to be appended
-    for i, file in enumerate(file_list):
-        print(f'{i+1:>3}/{len(file_list)}: {file}')
-        new: NDArray = np.load(file)
-        current_mask = preselector.get_next_segment(len(new))
-        all_data += new[current_mask].tolist()
-    print(f"INFO: saving as: {savename}")
-    np.save(savename, np.array(all_data))
-
-
-def process_args() -> Tuple[Sequence[Filename], Filename, str, Optional[Preselector]]:
+def process_args() -> Tuple[Sequence[Filename], Filename, str]:
     """set up the ArgParser, check if inputs are valid and do some processing"""
     parser = argparse.ArgumentParser(description='combine dataframes or rechits into one large file',
                                      prog='combine_files.py')
     parser.add_argument('files', nargs='+', help='file to be read')
     parser.add_argument('--outname', required=True, help='name of the outputfile')
     parser.add_argument('--list', action='store_true', help='use if input is a file with the filenames to be read from')
-    parser.add_argument('--preselection', help='filename, save only what matches the given preseection')
 
     args = parser.parse_args()
     files_: Sequence[Filename] = args.files
     outname_: Filename = args.outname
-    presectionfile: Optional[Filename] = args.preselection
     if args.list:
         files_ = np.loadtxt(files_[0], dtype=str)  # files is list with one element
     ending_ = file_ending(outname_)
     check_extensions(files_, extension=ending_)
-
-    preselection_: Optional[Preselector]
-    if presectionfile is not None:
-        preselection_ = Preselector(presectionfile)
-    else:
-        preselection_ = None
-    return files_, outname_, ending_, preselection_
-
+    return files_, outname_, ending_
 
 if __name__ == "__main__":
-    files, outname, ending, preselection = process_args()
-    # home_dir: str = "/home/home1/institut_3a/kappe/"
-    # savedir: str = home_dir + "work/data/"
-    # todo hardcoding is not smart
+    files, outname, ending = process_args()
+    savedir: str = ''
 
     if ending == 'pkl':
         combine_dataframes(files, outname)
     elif ending == 'npy':
-        if preselection is None: combine_rechits(files, outname)
-        else: combine_rechits_preselected(files, outname, preselection)
+        combine_rechits(files, outname)
     else:
         raise ValueError(f'file ending not supported: {ending}')
     print('Finished')
+
